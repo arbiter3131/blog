2d09617079b90ce317249b0fad5bf71bf8efa08c
j.lacour <j.lacour@cgi.com>
2019-06-24 09:24:51 +0300

    Refactor classes names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PostListComponentComponent } from './post-list-component/post-list-component.component';
-import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import { PostService } from './services/post.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponentComponent,
-    PostListItemComponentComponent
+    PostListComponent,
+    PostListItemComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
